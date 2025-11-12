28e4861f68fe1cf4a466232ec8e52b142ecb9d34
liabersiv <juliaribeiro.dsv@gmail.com>
2025-11-12 20:54:14 -0300

    message dialog

--- src/br/com/pi/ui/DialogHandler.java
+++ src/br/com/pi/ui/DialogHandler.java
@@ -1,8 +1,14 @@
 // Julia
 package br.com.pi.ui;
 
+import javax.swing.JOptionPane;
+
 public class DialogHandler {
     
     // TODO: Implementar caixas de diálogo (mensagens e confirmações)
+    public static void main (String[] args) {
+        JOptionPane.showMessageDialog(null, "Cadastro de cliente realizado com sucesso.");
+    }
+
     
 }
