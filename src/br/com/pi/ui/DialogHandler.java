10d2e92d8e9679a2f66d39d2c94e281726fc0a4d
liabersiv <juliaribeiro.dsv@gmail.com>
2025-11-12 21:03:50 -0300

    mensagens e confirmações - joptionpane

--- src/br/com/pi/ui/DialogHandler.java
+++ src/br/com/pi/ui/DialogHandler.java
@@ -8,7 +8,12 @@ public class DialogHandler {
     // TODO: Implementar caixas de diálogo (mensagens e confirmações)
     public static void main (String[] args) {
         JOptionPane.showMessageDialog(null, "Cadastro de cliente realizado com sucesso.");
+        JOptionPane.showMessageDialog(null, "Cadastro de produto realizado com sucesso.");
+
+        //parte que eu não coloquei no protótipo
+        JOptionPane.showConfirmDialog(null,"Deseja mesmo limpar tudo?", "Limpar tudo", JOptionPane.OK_CANCEL_OPTION);
     }
+
 
     
 }
